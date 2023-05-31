c0d4db32fccdd20970807d0e5502488a6194a28a
Traky Deng <trakydeng@gmail.com>
2023-05-31 17:33:04 +0800

    Add description to the output

--- filter-openapi/print-path-summary.py
+++ filter-openapi/print-path-summary.py
@@ -3,25 +3,38 @@ import json
 with open('admin-api.json') as f:
     openapi_dict = json.load(f)
 
-with open('path-summary.txt', 'w') as f:
-    paths = openapi_dict['paths']
-    for path_name, path_obj in paths.items():
-        f.write(f"Path: {path_name}\n")
-        for method_name, method_obj in path_obj.items():
-            if 'summary' in method_obj:
-                f.write(f"  {method_name}: {method_obj['summary']}\n")
-            else:
-                f.write(f"  {method_name}: No summary provided\n")
-        f.write('\n')
+def print_path_summ_desc():
+    with open('path-summary-desc.txt', 'w') as f:
+        paths = openapi_dict['paths']
+        for path_name, path_obj in paths.items():
+            f.write(f"Path: {path_name}\n")
+            for method_name, method_obj in path_obj.items():
+                f.write(f"  {method_name}:\n")
+                if 'summary' in method_obj:
+                    f.write(f"    Summary: {method_obj['summary']}\n")
+                else:
+                    f.write(f"    Summary: No summary provided\n")
+                if 'description' in method_obj:
+                    f.write(f"    Description: {method_obj['description']}\n")
+                else:
+                    f.write(f"    Description: No description provided\n")
+            f.write('\n')
 
+def main():
+    print_path_summ_desc()
+
+if __name__ == '__main__':
+    main()
+    
 ### Output format
-#
+
 # Path: /admin/routes
-#   get: Get All Routes
-#   post: Create Route
+#   get:
+#     Summary: xxx
+#     Description: xxx
+#   post:
+#     Summary: xxx
+#     Description: xxx
 #
 # Path: /admin/routes/{id}
-#   put: Create Route by ID
-#   get: Get Route by ID
-#   patch: Update Route
-#   delete: Delete Route by ID
+#   ...
